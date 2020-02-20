63e0673233070705525c5434a590caf287505aeb
Ankita Ahir <ankita.ahir@1rivet.local>
2020-02-20 15:24:43 +0530

    Add : Added my first project

---
+++ Employee.DTO/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.DTO
+{
+   public class EmployeeModel
+    {
+
+       
+        public int Experience { get; set; }
+        
+        public string MarriedStatus { get; set; }
+
+    }
+}
---
+++ EmployeeWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Department.DTO;
+using Employee.BLL;
+using Employee.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeManagement _employeeManagement;
+        private readonly IDepartmentManagement _departmentManagement;
+        EmployeeDetail employeeDetail = new EmployeeDetail();
+        readonly DepartmentDetail departmentDetail = new DepartmentDetail();
+        public EmployeeController(IEmployeeManagement employeeManagement, IDepartmentManagement departmentManagement)
+        {
+            _employeeManagement = employeeManagement;
+            _departmentManagement = departmentManagement;
+
+        }
+        [HttpGet]
+        public ActionResult<List<EmployeeDetail>> ShowEmployeeDetail()
+        {
+            List<EmployeeDetail> EmployeeDetail = _employeeManagement.ShowAllEmployeeDetail();
+            return EmployeeDetail;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<EmployeeDetail> GetEmployeeDetail(int EmployeeID)
+        {
+            EmployeeDetail employeeDetail = _employeeManagement.GetSingleEmployeeDetail(EmployeeID);
+            return employeeDetail;
+        }
+
+        [HttpPost]
+        public EmployeeDetail AddEmployeeDetail([FromBody]EmployeeDetail employeeDetail)
+        {
+            if (employeeDetail != null)
+            {
+                _employeeManagement.AddEmployeeDetail(employeeDetail);
+            }
+            return employeeDetail;
+        }
+
+        [HttpPut("{EmployeeID}")]
+        public string UpdateEmployeeDetail( int EmployeeID, [FromBody] EmployeeModel employeeModel)
+        {
+            _employeeManagement.UpdateEmployeeDetail(EmployeeID, employeeModel.Experience, employeeModel.MarriedStatus);
+            return "update successfully";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteEmployeeDetail(int EmployeeID)
+        {
+            _employeeManagement.DeleteEmployeeDetail(EmployeeID);
+            return "Deleted successfully";
+        }
+
+
+
+
+    }
+}
